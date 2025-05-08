b25c3d15968d802228afba8330fb3115e4c7a519
Esp <demid2207@gmail.com>
2025-05-08 15:23:20 +0300

    New system of modes and added subscriptions for upper models

--- bot/handlers/callback_handler.py
+++ bot/handlers/callback_handler.py
@@ -12,16 +12,16 @@ async def call_model(call: CallbackQuery):
     msg = await exsuf_model(call.data)
     sub = await get_subscribe(chat_id)
     if sub is None:
-        await call.answer(f"Пустой ответ")
+        await call.answer(f"Пустой ответ", show_alert=True)
         return
     if models[msg] not in subs[sub[0]]:
-        await call.answer(f"Вам недоступна эта модель. Купите подписку для получения новых моделей.")
+        await call.answer(f"Вам недоступна эта модель. Купите подписку для получения новых моделей.", show_alert=True)
     else:
         answer = await set_model(chat_id, msg)
         if answer:
-            await call.answer(f"Модель ИИ изменена на {answer}")
+            await call.answer(f"Модель ИИ изменена на {answer}", show_alert=True)
         else:
-            await call.answer(f"У вас уже выбрана эта модель")
+            await call.answer(f"У вас уже выбрана эта модель", show_alert=True)
     await call.message.delete()
 
 async def call_subscription(call: CallbackQuery):
@@ -30,14 +30,14 @@ async def call_subscription(call: CallbackQuery):
     msg = await exsuf_sub(call.data)
     sub = await get_subscribe(chat_id)
     if await subs_check(sub[0], msg[0]) < 0:
-        await call.answer(f"Вы не можете купить подписку ниже вашей.")
+        await call.answer(f"Вы не можете купить подписку ниже вашей.", show_alert=True)
     else:
         answer = await payment_sub(chat_id, msg[0], int(msg[1]))
         if answer:
             if msg[0] == answer[1]:
-                await call.answer(f"Отлично! Ваша подписка {answer[1]} продлена до {answer[0]}.")
+                await call.answer(f"Отлично! Ваша подписка {answer[1]} продлена до {answer[0]}.", show_alert=True)
             else:
-                await call.answer(f"Отлично! Вам выдана подписка {answer[1]} до {answer[0]}")
+                await call.answer(f"Отлично! Вам выдана подписка {answer[1]} до {answer[0]}", show_alert=True)
         else:
-            await call.answer(f"Ошибка оплаты")
+            await call.answer(f"Ошибка оплаты", show_alert=True)
     await call.message.delete()
